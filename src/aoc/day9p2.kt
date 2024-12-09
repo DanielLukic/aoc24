fc6dc3f5daff3714f678fe71bb8b629514f26594
Daniel Lukic <daniel.lukic@gmx.net>
2024-12-09 11:49:18 +0100

    Fix day 9 part 2

--- src/aoc/day9p2.kt
+++ src/aoc/day9p2.kt
@@ -1,12 +1,12 @@
 package aoc;
 
 import java.io.File
+import java.math.BigInteger
 
 private data class Entry(var pos: Int, val id: Int, var size: Int)
 
 fun main() {
     val input = File("day9.txt").readLines().single()
-    println(input)
     var id = 0
     var file = true
 
@@ -29,8 +29,10 @@ fun main() {
 
     val freed = mutableListOf<Entry>()
     for (f in used.reversed()) {
+        println("fit ${f.size}")
         val space = free.firstOrNull { it.size >= f.size }
         if (space == null) continue
+        if (space.pos > f.pos) continue
         freed.add(Entry(f.pos, -1, f.size))
         f.pos = space.pos
         space.size -= f.size
@@ -44,13 +46,12 @@ fun main() {
         repeat(e.size) { blocks.add(e.id) }
     }
 
-    println(blocks.joinToString("") { if (it == -1) "." else it.toString() })
+    println(blocks.joinToString("") { if (it == -1) "." else "x" })
 
-    var checksum = (0L).toULong()
+    var checksum = BigInteger.valueOf(0)
     for (p in blocks.indices) {
         if (blocks[p] == -1) continue
-        println("add pos $p times id ${blocks[p]}")
-        checksum += blocks[p].toULong() * p.toULong()
+        checksum += BigInteger.valueOf(blocks[p].toLong() * p.toLong())
     }
     println(checksum)
 }
