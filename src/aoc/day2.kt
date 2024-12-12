a53a0ffb6ca7b078e16fff0b46cf3de0de36c372
Daniel Lukic <daniel.lukic@gmx.net>
2024-12-12 05:17:36 +0100

    Refactor day 2 to clouddjr solution

--- src/aoc/day2.kt
+++ src/aoc/day2.kt
@@ -6,9 +6,14 @@ fun main() {
     val input = File("day2.txt").readLines();
     val reports = input.map { it.split(" ").map(String::toInt) }
     println("part 1: ${reports.count { isSafe(it) }}")
+    println("part 1: ${reports.count { isSafeWithCorrection(it) }}")
 }
 
 private fun isSafe(report: List<Int>): Boolean {
     val diffs = report.zipWithNext { a, b -> b - a }
     return diffs.all { it in 1..3 } || diffs.all { it in -3..-1 }
 }
+
+private fun isSafeWithCorrection(report: List<Int>) = report.indices
+    .map { skip -> report.filterIndexed { index, _ -> index != skip } }
+    .any { isSafe(it) }
