7b769d11dd8a2310d90a7c73a09ad5e22cd62f91
Daniel Lukic <daniel.lukic@gmx.net>
2024-12-12 04:59:14 +0100

    Refactor to clouddjr solution

--- src/aoc/day2p1.kt
+++ src/aoc/day2p1.kt
@@ -1,24 +1,14 @@
 package aoc;
 
 import java.io.File
-import kotlin.math.absoluteValue
-import kotlin.math.sign
-
-private fun isSafe(report: List<Int>): Boolean {
-    val pairs = report.windowed(2);
-    val diffs = pairs.map { it.last() - it.first() }
-    val sign = diffs.first().sign;
-    for (i in diffs) {
-        if (i == 0) return false;
-        if (i.absoluteValue > 3) return false;
-        if (i.sign != sign) return false;
-    }
-    return true;
-}
 
 fun main() {
-    val i1 = File("day2.txt").readLines();
-    val reports = i1.map { it.split(" ").map(String::toInt) }
-    val safe = reports.filter { isSafe(it) };
-    println(safe.count());
+    val input = File("day2.txt").readLines();
+    val reports = input.map { it.split(" ").map(String::toInt) }
+    println("part 1: ${reports.count { isSafe(it) }}")
+}
+
+private fun isSafe(report: List<Int>): Boolean {
+    val diffs = report.zipWithNext { a, b -> b - a }
+    return diffs.all { it in 1..3 } || diffs.all { it in -3..-1 }
 }
